adaec16e82a40e6be097433abf2119087b368734
user <user@192.168.0.7>
2020-07-30 14:43:11 +0530

    spring crud

---
+++ SpringRestfulWebApi-1/bin/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+insert into avatar values (101,'Akash','apple');
+insert into avatar values (102,'vikash','vivo');
+insert into avatar values (103,'Abd','oneplus');
+insert into avatar values (104,'sunny','samsung');
+insert into avatar values (105,'john','oppo');
